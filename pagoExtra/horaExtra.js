7d60665072b8f89c8f6e705954097aeb49da219c
mauricio noscue <mauronoscue@gmail.com>
2024-04-25 15:16:51 -0500

    hola

--- pagoExtra/horaExtra.js
+++ pagoExtra/horaExtra.js
@@ -4,14 +4,15 @@
     let resultado = "";
     if(horasTrabajadas>= 40){
         pagoExtra = (horasTrabajadas-40) *pagoHora * 1.5;
-        resultado += "Pago por horas extras : "+ pagoExtra;
+        alert(resultado += "Pago por horas extras : "+ pagoExtra);
     }else{
-        console.log("No hay horas extras trabajadas")
+        alert("No hay horas extras trabajadas")
     }
+    
 return resultado;
    }
         
 
 
 
-   
+      
