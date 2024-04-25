b7fe49ba1f19df4b37a08101108f8307542a5c77
SENA <SENA@NEIDFPSITFSD266.FormacionSena.RED>
2024-04-25 16:57:40 -0500

    areas

---
+++ AREAS DE FIGURAS 2/js/areas.js
@@ -0,0 +1,34 @@
+/**
+ * saludo - AREAS 
+ * autor: DaniEL bata
+*/
+
+function areaCuadrado(Plado){
+    let lado = Plado; 
+    
+    let resultado1;
+      resultado1= lado * lado;     
+      alert("El  area del cuadrado es : "+ resultado1)                 
+    return resultado1;
+}
+function areaRectangulo(Plado2,Paltura){
+    let lado = Plado2; 
+    let altura = Paltura
+    
+    let resultado2 
+        resultado2= lado * altura;
+        alert("El area del rectangulo es : "+ resultado2)                    
+    return resultado2;
+}
+function areaTriangulo(Plado3,Paltura){
+    let lado = Plado3; 
+    let altura = Paltura
+    let resultado3
+    
+    resultado3= (lado * altura)/2;
+    alert( "El area del triangulo es : "+ resultado3)                     
+    return resultado3;
+}
+
+ 
+
