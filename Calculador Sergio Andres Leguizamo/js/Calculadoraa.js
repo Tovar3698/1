f984254c89a6ad2106256d0e462ca4beff14dded
SENA <SENA@NEIDFPSITFSD764.FormacionSena.RED>
2024-04-25 14:55:14 -0500

    Calculadora2

--- Calculador Sergio Andres Leguizamo/js/Calculadoraa.js
+++ Calculador Sergio Andres Leguizamo/js/Calculadoraa.js
@@ -1,26 +1,21 @@
-function suma(pnum1,pnum2){
-    let num1 = pnum1;
-    let num2 = pnum2;
-    let sumar = num1 + num2;
-    return sumar
+function suma(num1, num2) {
+    let sumar = num1+num2;
+    return sumar;
 }
 
-function resta(pnum1,pnum2){
-    let num1 = pnum1;
-    let num2 = pnum2;
+function resta(num1,num2){
+
     let restar = num1 - num2;
-    return restar
+    return restar;
 
 }
-function multiplicacion(pnum1,pnum2){
-    let num1 = pnum1;
-    let num2 = pnum2;
+function multiplicacion(num1,num2){
+    
     let multiplicar = num1 * num2;
-    return multiplicar
+    return multiplicar;
 }
-function division(pnum1,pnum2){
-    let num1 = pnum1;
-    let num2 = pnum2;
+function division(num1,num2){
+    
     let dividir = num1 / num2;
-    return dividir
+    return dividir;
 }
